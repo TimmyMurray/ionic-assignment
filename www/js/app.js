94a6f039e1dd3109a314f12f3982aeac638afa4b
Timmy <g00183087@gmit.ie>
2015-04-16 15:51:24 +0000

    Added Label And Count Button

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,4 @@
-angular.module('calorific', ['ionic'])
-
-.run(function($ionicPlatform) {
+angular.module('calorific', ['ionic']) .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -12,3 +10,16 @@ angular.module('calorific', ['ionic'])
     }
   });
 })
+angular.module('calorific', ['ionic']) .controller('AppCtrl', function($scope) {
+  $scope.data = {};
+  
+  $scope.items = [
+    { price: '$4.99', text: 'Meat' },
+    { price: '$2.99', text: 'Dairy' },
+    { price: '$3.99', text: 'Fruit' },
+  ];
+    
+  $scope.clearSearch = function() {
+    $scope.data.searchQuery = '';
+  };
+});
